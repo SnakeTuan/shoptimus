94a59234ec9cea7c8d216c3a5e6a1bc86caf8194
SnakeTuan <snaketuan@gmail.com>
2025-02-16 14:33:19 +0700

    UI for store + connect to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
-import { Sidebar } from "@/components/Sidebar";
-import { Breadcrumbs } from "@/components/Breadcrumbs";
-import { ProductList } from "@/components/ProductList";
+import Link from "next/link";
 
-export default function Dashboard() {
+export default function Home() {
   return (
-    <div className="flex min-h-screen bg-gray-900 text-white">
-      <Sidebar />
-      <main className="flex-1 p-6">
-        <Breadcrumbs category="Electronics" />
-        <h2 className="mb-4 mt-4 text-2xl font-semibold">Product Management</h2>
-        <ProductList />
+    <div className="flex min-h-screen flex-col bg-gray-900 text-white">
+      <main className="flex flex-grow items-center justify-center">
+        <div className="px-4 text-center sm:px-6 lg:px-8">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight sm:text-5xl">
+            Shoptimus
+          </h1>
+          <p className="mx-auto mb-8 max-w-2xl text-xl text-gray-300 sm:text-2xl">
+            Simplify your offline store management with our powerful and
+            intuitive app.
+          </p>
+          <Link
+            href="/stores"
+            className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-black transition duration-150 ease-in-out hover:bg-gray-200"
+          >
+            Get Started
+          </Link>
+        </div>
       </main>
+
+      <footer className="py-4">
+        <p className="text-center text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} Shoptimus. All rights reserved.
+        </p>
+      </footer>
     </div>
   );
 }
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
-import { drizzle } from "drizzle-orm/neon-http";
-
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 // import { env } from "@/env";
 // import * as schema from "./schema";
 
@@ -7,4 +7,8 @@ import { drizzle } from "drizzle-orm/neon-http";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-export const db = drizzle(process.env.DATABASE_URL! as any);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool);
